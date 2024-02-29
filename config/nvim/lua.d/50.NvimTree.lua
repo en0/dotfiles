4c32323f20a3fd0435d926fcd44542210ad157a3
Ian Laird <irlaird@gmail.com>
2024-02-29 16:21:39 -0700

    I give up on regolith... it was bloated.

--- config/nvim/lua.d/50.NvimTree.lua
+++ config/nvim/lua.d/50.NvimTree.lua
@@ -52,16 +52,8 @@ require("nvim-tree").setup({
 
 
   view = {
-
     width = 30,
-    hide_root_folder = false,
     side = 'left',
-
-    mappings = {
-      custom_only = false,
-      list = {}
-    }
-
   },
 
   renderer = {
