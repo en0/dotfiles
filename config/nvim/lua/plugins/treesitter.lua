13327f967b32148798826eb1dc1e8ac2884b1072
Ian Laird <irlaird@gmail.com>
2026-09-25 21:02:56 -0700

    Local tweaks: journal keybinds, treesitter new API, nvim-tree cleanup, minion alias

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,19 +1,17 @@
 return {
     'nvim-treesitter/nvim-treesitter',
+    branch = 'main',
+    lazy = false,
+    build = ':TSUpdate',
     config = function()
-        require('nvim-treesitter.configs').setup({
-            ensure_installed = {
-                "asm", "bash", "c", "c_sharp", "cmake", "cpp", "css", "csv", "desktop", "diff",
-                "disassembly", "dockerfile", "git_config", "git_rebase", "gitcommit", "gitignore",
-                "go", "helm", "hcl", "html", "ini", "java", "javascript", "jinja", "jinja_inline",
-                "jq", "json", "lua", "markdown", "markdown_inline", "nginx", "objdump", "passwd",
-                "php", "printf", "properties", "python", "regex", "rego", "robots", "rust",
-                "scala", "scss", "sql", "ssh_config", "strace", "terraform", "toml", "vimdoc",
-                "xml", "yaml", "yang",
-            },
-            sync_install = true,
-            auto_install = true,
-            highlight = {enable = true}
-        })
+        require('nvim-treesitter').install {
+            "asm", "bash", "c", "c_sharp", "cmake", "cpp", "css", "csv", "desktop", "diff",
+            "disassembly", "dockerfile", "git_config", "git_rebase", "gitcommit", "gitignore",
+            "go", "helm", "hcl", "html", "ini", "java", "javascript", "jinja", "jinja_inline",
+            "jq", "json", "lua", "markdown", "markdown_inline", "nginx", "objdump", "passwd",
+            "php", "printf", "properties", "python", "regex", "rego", "robots_txt", "rust",
+            "scala", "scss", "sql", "ssh_config", "strace", "terraform", "toml", "vimdoc",
+            "xml", "yaml", "yang",
+        }
     end
 }
